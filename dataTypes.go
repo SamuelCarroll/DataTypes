2ca83c8283a28fff4b9ee737c09ea3f9f353f97f
samuel carroll <sam2@blackhillsinfosec.com>
2018-03-05 15:17:31 -0700

    adding prediciton interface for class

--- dataTypes.go
+++ dataTypes.go
@@ -3,6 +3,7 @@ package dataTypes
 //Data Basic data type that can hold any supervised learner type
 type Data struct {
 	Class        int
+	Prediction   int
 	UID          string
 	FeatureSlice []interface{}
 }
